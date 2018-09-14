54d8925f36e102b192a7345541c817945ec4744c
mallow <mallow@MacBook-Pro.local>
2018-09-14 18:54:51 +0800

    docs: add webpack.md

--- source/software/mac.md
+++ source/software/mac.md
@@ -3,6 +3,15 @@ title: mac相关
 type: software
 order: 5
 ---
+## 更新npm和node版本
+
+- 更新npm的方法是：npm update -g
+
+- 更新node需要两步：
+  - 1 -> `sudo npm install -g n`
+  - 2 -> `sudo n latest`
+
+
 ## vscode输入法中英文不能顺利切换
 
 - 今天竟然发现是主题的原因，额
---
+++ test/index.js
@@ -0,0 +1,28 @@
+function code() {
+  const path = require('path')
+  console.log(path.resolve(__dirname))
+  console.log(__dirname)
+  console.log(!function test(a) {
+    console.log(a)
+    return true
+  }('test'))
+  require('./models/autoFn')
+}
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log('<-- code start -->')
+  code()
+  console.log('<-- code end -->')
+});
